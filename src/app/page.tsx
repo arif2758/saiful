73a7c668ef39482f439b384daceee8efde60bb42
arif2758 <arif2758@gmail.com>
2025-07-19 15:18:41 +0600

    testing 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import FeaturdProjects from "@/components/FeaturdProjects";
 import HeroSection from "@/components/HeroSection";
+import LoadingSpin from "@/components/LoadingSpin";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div>
+    <Suspense fallback={<LoadingSpin />}>
       <HeroSection />
       <Skills />
       <FeaturdProjects />
       <Testimonials />/
-    </div>
+    </Suspense>
   );
 }
